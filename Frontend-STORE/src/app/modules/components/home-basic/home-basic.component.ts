3204ae9e1867aeecbd208e1731f671e6db6d8a18
NicolasC22G <ncastellanos@sodimac.vc-soft.com>
2024-12-30 10:59:03 -0500

    ADD-REGEX- Se agrega validador de regex para mail sonar

--- Frontend-STORE/src/app/modules/components/home-basic/home-basic.component.ts
+++ Frontend-STORE/src/app/modules/components/home-basic/home-basic.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeBasicComponent implements OnInit {
 
+  mailRegexpValidation = new RegExp(/^([-_0-9a-z]+[\.])*[-_0-9a-z]+@[-_0-9a-z]*([-_0-9a-z][\.]?)+[-_0-9a-z](\.[-_0-9a-z]+)$/);
+
   constructor() { }
 
   ngOnInit() {
